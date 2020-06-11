afc4d6444db3ac0b849b0ebe50fe5cf2eeb99d66
Sixu Hu <husixu1@hotmail.com>
2020-06-11 17:23:03 +0800

    sentiment: update script

--- sentiment/train_dp.sh
+++ sentiment/train_dp.sh
@@ -10,8 +10,10 @@ for bpl in 1 2 3 5 10 20 40 80 160 320 640; do
     python -u ./sentiment-federated-dp.py -E 20 -l $bpl -e 2.0 | tee -a "./result_dp/epochs_20_batchsize_40_bpl_""$bpl""_e_2.0"
 done
 
+mkdir -p result_dp_single
+
 for dataset in imdb amazon; do
     for bpl in 1 2 3 5 10 20 40 80 160 320 640; do
-        python -u ./sentiment-dp.py -E 20 -D $dataset -l $bpl -e 2.0 | tee -a "./result_dp_single/""$dataset""_batchsize_40_bpl_""$bpl""_e_""$epsilon"
+        python -u ./sentiment-dp.py -E 20 -D $dataset -l $bpl -e 2.0 | tee -a "./result_dp_single/""$dataset""_batchsize_40_bpl_""$bpl""_e_2.0"
     done
 done
