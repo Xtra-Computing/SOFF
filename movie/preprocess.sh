d4b26765b56c88cd0b9645399e48dcaa8d5d1f63
yuanlics <55344711+yuanlics@users.noreply.github.com>
2021-03-22 22:43:11 +0800

    Update preprocess.sh

--- movie/preprocess.sh
+++ movie/preprocess.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-wget -nc http://files.grouplens.org/datasets/movielens/ml-1m.zip
-unzip ml-1m.zip
+# wget -nc http://files.grouplens.org/datasets/movielens/ml-1m.zip
+# unzip ml-1m.zip
 
-mkdir -p imdb
-pushd imdb
+# mkdir -p imdb
+# pushd imdb
+# wget -nc https://datasets.imdbws.com/name.basics.tsv.gz
+# wget -nc https://datasets.imdbws.com/title.akas.tsv.gz
+# wget -nc https://datasets.imdbws.com/title.basics.tsv.gz
+# wget -nc https://datasets.imdbws.com/title.crew.tsv.gz
+# wget -nc https://datasets.imdbws.com/title.episode.tsv.gz
+# wget -nc https://datasets.imdbws.com/title.principals.tsv.gz
+# wget -nc https://datasets.imdbws.com/title.ratings.tsv.gz
+# gzip -d name.basics.tsv.gz
+# gzip -d title.akas.tsv.gz
+# gzip -d title.basics.tsv.gz
+# gzip -d title.crew.tsv.gz
+# gzip -d title.episode.tsv.gz
+# gzip -d title.principals.tsv.gz
+# gzip -d title.ratings.tsv.gz
+# popd
 
-wget -nc https://datasets.imdbws.com/name.basics.tsv.gz
-wget -nc https://datasets.imdbws.com/title.akas.tsv.gz
-wget -nc https://datasets.imdbws.com/title.basics.tsv.gz
-wget -nc https://datasets.imdbws.com/title.crew.tsv.gz
-wget -nc https://datasets.imdbws.com/title.episode.tsv.gz
-wget -nc https://datasets.imdbws.com/title.principals.tsv.gz
-wget -nc https://datasets.imdbws.com/title.ratings.tsv.gz
-gzip -d name.basics.tsv.gz
-gzip -d title.akas.tsv.gz
-gzip -d title.basics.tsv.gz
-gzip -d title.crew.tsv.gz
-gzip -d title.episode.tsv.gz
-gzip -d title.principals.tsv.gz
-gzip -d title.ratings.tsv.gz
-
-popd
+echo "Please download the files from https://drive.google.com/drive/folders/15rLG87D1OdIScDUam1Ue9YgV2OgKxXHG?usp=sharing"
+echo "And then put the csv files in this directory."
